7ab4a9b730b3965e8b9713aab95835f1f45682f5
Jason Gilmore <32154076+JasonGilmore@users.noreply.github.com>
2025-11-14 21:09:38 +1100

    Add basic visit counting per content page

--- server.js
+++ server.js
@@ -1,20 +1,49 @@
+const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
 const utils = require('./src/utils.js');
 
+// Prepare visit counter
+const visitCountFilePath = path.join(__dirname, 'visitCounter.json');
+if (!fs.existsSync(visitCountFilePath)) {
+    fs.writeFileSync(visitCountFilePath, JSON.stringify({}));
+}
+const visitCounter = JSON.parse(fs.readFileSync(visitCountFilePath));
+
+function incrementSiteVisit(contentType, pageName) {
+    if (!visitCounter[contentType]) {
+        visitCounter[contentType] = {};
+    }
+    visitCounter[contentType][pageName] = (visitCounter[contentType][pageName] || 0) + 1;
+    fs.writeFile(visitCountFilePath, JSON.stringify(visitCounter, null, 4), (err) => {
+        if (err) {
+            console.error('Error writing counter file: ', err);
+        }
+    });
+}
+
 utils.validateConfigurations();
 const contentTypes = Object.keys(utils.siteConfig.content);
 
-// Rewrite content paths
+// Rewrite content paths and count visits
 // When "/recipes/bread" serve "/public/recipes/bread/bread.html"
 app.use((req, res, next) => {
     const noExt = !path.extname(req.path);
-    const isContent = contentTypes.some((contentType) => req.path.startsWith(`/${contentType}`));
+    let matchedContentType;
+    const isContent = contentTypes.some((contentType) => {
+        if (req.path.startsWith(`/${contentType}`)) {
+            matchedContentType = contentType;
+            return true;
+        }
+        return false;
+    });
+
     const reqParts = req.path.split('/').filter(Boolean);
     if (noExt && isContent && reqParts.length > 1) {
         const contentItem = reqParts[reqParts.length - 1];
+        incrementSiteVisit(matchedContentType, contentItem);
         req.url = `${req.url}/${contentItem}.html`;
     }
     next();
